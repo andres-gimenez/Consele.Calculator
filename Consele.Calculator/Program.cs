814b946bfc6f9c76eeb868e25e82c6dedcaabde2
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2021-09-27 18:24:13 +0200

    Add project files.

---
+++ Consele.Calculator/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Consele.Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 1 && args[0] == "--help")
+            {
+                ShowHelp();
+            }
+            else if(args.Length != 3)
+            {
+                ShowError();
+                return;
+            }
+
+            var command = args[0];
+
+            double.TryParse(args[1], out double value1);
+
+            double.TryParse(args[2], out double value2);
+
+            switch (command)
+            {
+                case "--add":
+                    Add(value1, value2);
+                    break;
+                case "--sub":
+                    Sub(value1, value2);
+                    break;
+                case "--plus":
+                    Plus(value1, value2);
+                    break;
+                case "--div":
+                    Div(value1, value2);
+                    break;
+                default:
+                    ShowError();
+                    break;
+            }
+        }
+
+        private static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a:N} + {b:N} = {a + b:N}");
+        }
+
+        private static void Sub(double a, double b)
+        {
+            Console.WriteLine($"{a:N} - {b:N} = {a - b:N}");
+        }
+
+        private static void Plus(double a, double b)
+        {
+            Console.WriteLine($"{a:N} * {b:N} = {a * b:N}");
+        }
+
+        private static void Div(double a, double b)
+        {
+            if (b == 0.0)
+                Console.WriteLine("No se dividir entre 0");
+            else
+                Console.WriteLine($"{a:N} / {b:N} = {a / b:N}");
+        }
+
+        private static void ShowError()
+        {
+            Console.WriteLine("Error: linea de comando mal formada.");
+            ShowHelp();
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("USO:");
+
+            Console.WriteLine("ConsoleCalculator [comand] [value1] [value2]");
+
+            Console.WriteLine();
+
+            Console.WriteLine("donde");
+            Console.WriteLine("comand \t Comando add, sub, plus, div");
+            Console.WriteLine();
+
+        }
+    }
+}
