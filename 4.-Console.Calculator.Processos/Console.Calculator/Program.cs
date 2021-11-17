c05792fcbc072f36d1a92abcc5d0248c48b61edc
andres.gimenez <andres.gimenez@outlook.com>
2021-11-17 08:35:37 +0100

    Modificación de texto

--- 4.-Console.Calculator.Processos/Console.Calculator/Program.cs
+++ 4.-Console.Calculator.Processos/Console.Calculator/Program.cs
@@ -78,7 +78,7 @@ namespace Calculator
 
             if (process.ExitCode == 0)
             {
-                Console.WriteLine("El proceso finalizó con exito.");
+                Console.WriteLine("El proceso ha finalizado.");
                 Console.WriteLine(outputWriter.ReadToEnd());
             }
             else
